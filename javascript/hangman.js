96d7b32d69150a850624b4d9ea9a16aec4975bcc
eanzalone <emily.anzalone@gmail.com>
2017-05-20 14:22:56 -0700

    prompt for guesses

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -15,20 +15,31 @@ for (var i=0; i<word.length; i++) {
 }
 console.log(answerArray);
 // guesses left to 10
+var remainingGueses = 10;
+//set remaining letters to word length
+var remainingLetters =  word.length;
 
 // while word has not been guessed and still have guesses
+while (remainingLetters > 0 && remainingGueses > 0) {
     // show progress
+    alert(answerArray.join(" "));
     // prompt guess
+    var guess = prompt("Guess a letter, or click cancel to stop playing");
 
     // if the player wants to quit
+    if (guess == null) {
         // Quit
+        break;
+    // else if the guess is not a single letter
+    } else if (guess.length != 1) {
+        // tell player to pick a single letter
+        alert("Please enter a single letter");
+    }
     
-    // else if the guess is not a single WEBGL_depth_texture
-    //     tell player to pick a single WEBGL_depth_texture
-
     // else
     //     if the guess is in the word,
     //     update progress
 
     // if the player has used all guesses
     //     game over
+}
